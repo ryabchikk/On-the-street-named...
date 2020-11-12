b49034c1a7ce36b754825bcfe00d262a1626e8bc
Mixail <mixail-12-r@mail.ru>
2020-11-12 14:51:24 +0000

    No Comments

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OnclickExit()
+    {
+        Application.Quit();
+    }
+}
