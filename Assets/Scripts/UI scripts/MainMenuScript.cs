c714ede20a50d97ee9be908a7887a5c3ebfd845c
Mixail <mixail-12-r@mail.ru>
2021-02-27 17:58:02 +0000

    добавил изменение разрешения экрана в настройках главного меню

--- Assets/Scripts/UI scripts/MainMenuScript.cs
+++ Assets/Scripts/UI scripts/MainMenuScript.cs
@@ -7,19 +7,35 @@ using System.Linq;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public Dropdown _dropdown;
-    
+    //интерфейс настройки графики
+    public Dropdown _dropdown1;
+    //интерфейс разрешения экрана
+    public Dropdown _dropdown2;
+    //массив разрешений экрана
+    Resolution[] _resolution;
     void Start()
     {
-        _dropdown.AddOptions(QualitySettings.names.ToList());
-        _dropdown.value = QualitySettings.GetQualityLevel();
+        _dropdown1.AddOptions(QualitySettings.names.ToList());
+        _dropdown1.value = QualitySettings.GetQualityLevel();
+        _resolution=Screen.resolutions;
+        string[] strRes = new string[_resolution.Length];
+        for (int i = 0; i < _resolution.Length; i++) 
+        {
+            strRes[i] = _resolution[i].ToString();
+        }
+        _dropdown2.AddOptions(strRes.ToList());
+
+        Screen.SetResolution(_resolution[_resolution.Length-1].width,_resolution[_resolution.Length-1].height,true);
     }
     
     public void SetQualitu() 
     {
-        QualitySettings.SetQualityLevel(_dropdown.value);
+        QualitySettings.SetQualityLevel(_dropdown1.value);
     }
-    
+    public void SetRes() 
+    {
+        Screen.SetResolution(_resolution[_dropdown2.value].width, _resolution[_dropdown2.value].height, true);
+    }
     public void OnClickStart()
     {
         Time.timeScale = 1;
