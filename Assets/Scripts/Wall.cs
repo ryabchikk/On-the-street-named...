9990cf41c23288bb1f4e0088a3ca6980e95610dc
kiryaT <kirya.kirchak@mail.ru>
2021-03-30 19:21:25 +0000

    добавлены враги!

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    
+    [SerializeField] private Collider wall; 
+   
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       
+
+        if (collision.collider.GetComponent<CheckPlayer>()!= null)
+        {
+            wall.enabled = false;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        
+
+        if (collision.collider.GetComponent<CheckPlayer>() != null)
+        {
+            wall.enabled = true ;
+        }
+    }
+}
