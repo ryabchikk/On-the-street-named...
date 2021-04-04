b54a5b1a2bae2018370c446daadd4fc2be42f5df
Mixail <mixail-12-r@mail.ru>
2021-04-04 20:14:51 +0000

    заменена модель портала замнена модель головоломки с морзе исправлен баг, когда при уничтожении врага на крыше перестают спавниться новые отредактирован terrain добавлен бестиарий врага создана "сюжетная сцена"

---
+++ Assets/Scripts/UI scripts/bestiaryMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bestiaryMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject bestiaryPanel;
+    private Player _player;
+    private void Start()
+    {
+        _player = Player.player;
+    }
+    public void BestiaryOn() 
+    {
+        _player.DeactivateShooting();
+        bestiaryPanel.SetActive(true);
+    }
+    public void BestiaryOff()
+    {
+        bestiaryPanel.SetActive(false);
+        _player.ActivateShooting();
+    }
+}
