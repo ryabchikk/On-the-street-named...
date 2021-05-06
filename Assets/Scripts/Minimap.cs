bbf497c88657ee20384403d3b98743bf38b6d681
Dmitry <x355x02c@live.com>
2021-05-06 12:56:12 +0000

    Добавлена миникарта, для использования нужно для каждого объекта который будет на миникарте отображаться создавать отдельный спрайт/3д объект с его отображением на миникарте и ставить на него слой Minimap. Minimap player и minimap terrain объекты и материалы к ним созданы для демонстрации, их можно удалить

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    private Transform _player;
+    private Transform _cachedTransform;
+    
+    private void Start()
+    {
+        _player = Player.player.transform;
+        _cachedTransform = transform;
+    }
+    
+    void Update()
+    {
+        _cachedTransform.position = new Vector3(_player.position.x, _cachedTransform.position.y, _player.position.z);
+    }
+}
